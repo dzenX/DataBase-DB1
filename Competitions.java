322eb300a4108218517db879a31055a2c8487f3d
nickita.skilkov <sklkvnv@gmail.com>
2017-09-11 11:49:11 +0300

    second step

---
+++ Competitions.java
@@ -0,0 +1,7 @@
+/**
+ * Created by spide on 11.09.2017.
+ */
+import java.util.ArrayList;
+
+public class Competitions {
+}
--- OlympicGame.java
+++ OlympicGame.java
@@ -1,5 +1,22 @@
 /**
  * Created by spide on 06.09.2017.
  */
+
 public class OlympicGame {
+    private int id;
+    private int year_of_game;
+    public OlympicGame(){
+        id = -1;
+        year_of_game = -1;
+    }
+    public OlympicGame(int _id, int _year_of_game){
+        id = _id;
+        year_of_game = _year_of_game;
+    }
+    public int getId(){
+        return id;
+    }
+    public int getYear(){
+        return year_of_game;
+    }
 }
